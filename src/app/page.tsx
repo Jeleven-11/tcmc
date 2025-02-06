571ff47a0ab6ca7ac310be6dbbe1bee4375a4771
Piscode1337 <princetonino.piscos@g.msuiit.edu.ph>
2025-02-06 12:47:56 +0800

    Time test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,15 @@
 import HomeClient from "@/components/HomeClient";
 //import NotificationButton from "@/components/NotificationButton";
 import PushNotification from "@/components/PushNotifications";
-
+import { DateTime } from "luxon";
 export default function Home()
 {
+  const asd = DateTime.now();
+  const asdd = new Date().toISOString();
   return (
     <>
+      { asd.toString() }
+      { asdd.toString() }
       <PushNotification />
       <HomeClient />
     </>
