91d83c1e2e867893f4c07271d2afeed266a2ef59
Piscode1337 <princetonino.piscos@g.msuiit.edu.ph>
2025-02-06 14:33:47 +0800

    UPDATE:     - Force dynamic of graph data
    -	modified:   src/app/api/getChartDailyReports/route.ts
    -	modified:   src/app/api/getChartWeeklyReports/route.ts

--- src/app/api/getChartDailyReports/route.ts
+++ src/app/api/getChartDailyReports/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { query } from "@/app/lib/db";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET()
 {
     const rows = await query(
