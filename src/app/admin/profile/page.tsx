776576ba6701c80a8d7679a9e0725194f8d6bb2b
Piscode1337 <princetonino.piscos@g.msuiit.edu.ph>
2025-02-08 09:29:06 +0800

    UPDATE: 	- Added /reportlist/ 	- Added notifications when changing status on reports status 	- Added master password to update user admin account (TODO: delete)     - Merged to last update 	- Transfered notification permissions to /admin/* 	- Changes for reports with carousel if owner = 'Yes' 	- Changes to delete report id -> ReportID 	- Changes to middleware instead of confirming the token if valid, instead, check of session and if the user admin is logged in.
    - 	modified:   .env
    - 	modified:   package-lock.json
    - 	modified:   package.json
    - 	modified:   public/sw.js
    - 	modified:   src/app/admin/page.tsx
    - 	modified:   src/app/admin/profile/page.tsx
    - 	new file:   src/app/admin/reportlist/page.tsx
    - 	modified:   src/app/admin/watchlist/page.tsx
    - 	modified:   src/app/adminlogin/page.tsx
    - 	modified:   src/app/api/login_auth/route.ts
    - 	new file:   src/app/api/masterLogin/route.ts
    - 	new file:   src/app/api/masterLogin/updateProfile/route.ts
    - 	modified:   src/app/api/reports/[id]/route.ts
    - 	modified:   src/app/api/reports/route.ts
    - 	modified:   src/app/api/sendPush/route.ts
    - 	modified:   src/app/api/subscribe/route.ts
    - 	modified:   src/app/layout.tsx
    - 	modified:   src/app/lib/db.ts
    - 	modified:   src/app/page.tsx
    - 	modified:   src/components/AddUserModal.tsx
    - 	modified:   src/components/LineWeeklyTinapa.tsx
    - 	deleted:    src/components/NotificationButton.tsx
    - 	modified:   src/components/PushNotifications.tsx
    - 	modified:   src/middleware.ts
    - 	modified:   src/utils/notifications.ts
    - 	modified:   src/utils/registerServiceWorker.ts

--- src/app/admin/profile/page.tsx
+++ src/app/admin/profile/page.tsx
@@ -97,7 +97,7 @@ export default function Profile()
     {
       const res = await fetch('/api/masterLogin/updateProfile',
       {
-        method: 'POST', // or 'PATCH' if updating
+        method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(editData),
       })
