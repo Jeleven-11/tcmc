141a2fe764f36c8d8851be3f84969da8cac46fc6
RMV-Coder <70832774+RMV-Coder@users.noreply.github.com>
2025-03-05 14:56:28 +0800

    Update route.ts

--- src/app/api/dont_fornow/route.ts
+++ src/app/api/dont_fornow/route.ts
@@ -1,10 +1,10 @@
-import { query } from "@/app/lib/db";
-import { NextResponse } from "next/server";
+// import { query } from "@/app/lib/db";
+// import { NextResponse } from "next/server";
 
-export async function GET()
-{
-    const asd = await query('SELECT * FROM master_password', [])
-    // const asda = JSON.parse(asd)
+// export async function GET()
+// {
+//     const asd = await query('SELECT * FROM master_password', [])
+//     // const asda = JSON.parse(asd)
 
-    return NextResponse.json({ asd }, { status: 200 })
-}
+//     return NextResponse.json({ asd }, { status: 200 })
+// }
