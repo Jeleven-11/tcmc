f4790681866aeb1d912c3a4e5ccbdb46afd93ce6
Jeleven-11 <yukikitazawa03@gmail.com>
2025-03-05 12:48:35 +0800

    Update route.ts

--- src/app/api/active-users/route.ts
+++ src/app/api/active-users/route.ts
@@ -6,6 +6,6 @@ export async function GET() {
         const activeUsers = getActiveUsers();
         return NextResponse.json(activeUsers);
     } catch (error) {
-        return NextResponse.json({ error: Error }, { status: 500 });
+        return NextResponse.json({ error: error instanceof Error ? error.message : 'Unknown error' }, { status: 500 });
     }
 }
