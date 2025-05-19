9b7df6f638bd0611eb4b2b24a49f5ebe5712ed79
RMV-Coder <70832774+RMV-Coder@users.noreply.github.com>
2025-05-19 09:22:03 +0800

    Update route.ts

--- src/app/api/reports/getReports/getReportsLazy/route.ts
+++ src/app/api/reports/getReports/getReportsLazy/route.ts
@@ -46,17 +46,17 @@ export async function GET(req: NextRequest)
             `SELECT * FROM reports ${whereClause} ORDER BY createdAt DESC LIMIT ? OFFSET ?`,
             queryParams
         );
-        // const isMultipleReported = await connection.query(`
-        //     SELECT t1.reportID, t1.platenumber,
-        //         CASE 
-        //             WHEN COUNT(t2.id) > 0 THEN 1
-        //             ELSE 0
-        //         END AS isMultiple
-        //     FROM reports t1
-        //     LEFT JOIN reports t2 ON t1.platenumber = t2.platenumber AND t1.id <> t2.id
-        //     WHERE t1.status = 'unread'
-        //     GROUP BY t1.reportID, t1.platenumber;
-        // `);
+        const isMultipleReported = await connection.query(`
+            SELECT t1.reportID, t1.platenumber,
+                CASE 
+                    WHEN COUNT(t2.id) > 0 THEN 1
+                    ELSE 0
+                END AS isMultiple
+            FROM reports t1
+            LEFT JOIN reports t2 ON t1.platenumber = t2.platenumber AND t1.id <> t2.id
+            WHERE t1.status = 'unread'
+            GROUP BY t1.reportID, t1.platenumber;
+        `);
         
         const countParams = queryParams.slice(0, -2); // Exclude pagination for COUNT query
         const total: [Counter[], FieldPacket[]] = await connection.query(
@@ -69,7 +69,7 @@ export async function GET(req: NextRequest)
         // const total = await query(`SELECT COUNT(*) as count FROM reports`, []) as { count: number }[]
         if (!total)
             return new Response('No reports found', { status: 404 })
-        return NextResponse.json({ data: reports[0], total: total[0][0].count}, { status: 200 })//isMultiple: isMultipleReported[0] 
+        return NextResponse.json({ data: reports[0], total: total[0][0].count, isMultiple: isMultipleReported[0] }, { status: 200 })
     } catch (error) {
         console.error('Error fetching reports:', error);
         return NextResponse.json({ error: 'Failed to fetch reports' }, { status: 500 });
